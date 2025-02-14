f175f719f6d959e95cae097aa288b01a004ae395
LutviP19 <lutvip19@gmail.com>
2025-02-14 10:06:12 +0700

    monitoring dashboard: telescope, pulse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,6 +43,18 @@
                                     >
                                         Admin
                                     </a>
+                                    <a
+                                        href="{{ url('/telescope') }}" target="_blank" 
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Telescope
+                                    </a>
+                                    <a
+                                        href="{{ url('/pulse') }}" target="_blank" 
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Pulse
+                                    </a>
                                 @else
                                     <a
                                         href="{{ route('login') }}"
