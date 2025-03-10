76d0c98ee388782bce0ea15051dd2f02376a6c39
LutviP19 <lutvip19@gmail.com>
2025-03-10 19:53:26 +0700

    replace mailhog to mailpit. add default reverb config

--- config/reverb.php
+++ config/reverb.php
@@ -71,11 +71,11 @@ return [
 
         'apps' => [
             [
-                'key' => env('REVERB_APP_KEY'),
-                'secret' => env('REVERB_APP_SECRET'),
-                'app_id' => env('REVERB_APP_ID'),
+                'key' => env('REVERB_APP_KEY', 'my-app-key'),
+                'secret' => env('REVERB_APP_SECRET', 'my-app-secret'),
+                'app_id' => env('REVERB_APP_ID', 'my-app-id'),
                 'options' => [
-                    'host' => env('REVERB_HOST'),
+                    'host' => env('REVERB_HOST', '127.0.0.1'),
                     'port' => env('REVERB_PORT', 443),
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
